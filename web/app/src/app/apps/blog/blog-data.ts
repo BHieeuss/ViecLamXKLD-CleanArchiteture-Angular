3bba4aa1935b10a261d639db55869af288a53a64
BHieeuss <tbh.11203@gmail.com>
2024-10-10 13:47:54 +0700

    Cc

--- web/app/src/app/apps/blog/blog-data.ts
+++ web/app/src/app/apps/blog/blog-data.ts
@@ -4,9 +4,9 @@ export const blogs: Blog[] = [
     id: 1,
     image: 'assets/images/blog/101.jpg',
     heading:
-      'Kỹ sư IT',
-    subHeading: 'Tuyển dụng 100 nhân sự IT tại Tokyo',
-    blogDate: 'Tháng 11, 2024',
+      'Magna nostrud incididunt cupidatat cillum incididunt et nisi in ut minim reprehenderit.',
+    subHeading: 'Labore irure irure laborum quis tempor aliqua. Dude',
+    blogDate: 'Feb 19, 2020',
     blogDetail:
       'Est ipsum sint officia quis nulla nisi cupidatat aliquip nisi laboris eiusmod eiusmod aliquip do. Commodo elit excepteur occaecat irure Lorem sit nulla nulla sint duis incididunt. Nostrud ut do mollit et amet velit aute excepteur et culpa culpa velit. Cillum veniam officia anim cupidatat.',
   },
@@ -83,16 +83,4 @@ export const blogs: Blog[] = [
     blogDetail:
       'Laboris culpa ea est et aliqua amet ullamco Lorem non enim laborum incididunt ex Lorem. Minim sint cupidatat magna esse labore id. Do qui incididunt consectetur nulla aliquip consequat nostrud incididunt occaecat ad. Excepteur nulla adipisicing enim ea occaecat fugiat dolore sunt est et. Eu veniam nostrud sit aute consectetur dolore ipsum fugiat anim duis.',
   },
-  {
-    id: 9,
-    image: 'assets/images/blog/909.jpg',
-    heading:
-      'Vasdrcitation occaecat.',
-    subHeading:
-      'Adipisicing sit minim occaecat nulla proident exercitation do laboris.',
-    blogDate: 'Feb 15, 2020',
-    blogDetail:
-      'Laboris culpa ea est et aliqua amet ullamco Lorem non enim laborum incididunt ex Lorem. Minim sint cupidatat magna esse labore id. Do qui incididunt consectetur nulla aliquip consequat nostrud incididunt occaecat ad. Excepteur nulla adipisicing enim ea occaecat fugiat dolore sunt est et. Eu veniam nostrud sit aute consectetur dolore ipsum fugiat anim duis.',
-  },
 ];
-
